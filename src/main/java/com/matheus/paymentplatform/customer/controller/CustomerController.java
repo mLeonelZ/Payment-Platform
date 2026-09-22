c777d38be3d076685e819aa83f012e6025b5edcb
mLeonelZ <matheus.leoonel@gmail.com>
2026-09-22 15:59:19 -0300

    feat: implement customer creation

---
+++ src/main/java/com/matheus/paymentplatform/customer/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.matheus.paymentplatform.customer.controller;
+
+import com.matheus.paymentplatform.customer.dto.CustomerRequest;
+import com.matheus.paymentplatform.customer.dto.CustomerResponse;
+import com.matheus.paymentplatform.customer.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    private final CustomerService service;
+
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> create(@RequestBody CustomerRequest request){
+        CustomerResponse response = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable Long id){
+        CustomerResponse response = service.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+}
---
+++ src/main/java/com/matheus/paymentplatform/customer/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.matheus.paymentplatform.customer.service;
+
+import com.matheus.paymentplatform.customer.domain.Customer;
+import com.matheus.paymentplatform.customer.dto.CustomerRequest;
+import com.matheus.paymentplatform.customer.dto.CustomerResponse;
+import com.matheus.paymentplatform.customer.exception.CustomerNotFoundException;
+import com.matheus.paymentplatform.customer.mapper.CustomerMapper;
+import com.matheus.paymentplatform.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository repository;
+    private final CustomerMapper mapper;
+
+    public CustomerService(CustomerRepository repository, CustomerMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public CustomerResponse create(CustomerRequest request){
+        Customer customer = mapper.toEntity(request);
+        Customer savedCustomer = repository.save(customer);
+
+        return mapper.toResponse(savedCustomer);
+    }
+
+    public CustomerResponse findById(Long id){
+        Customer customer = repository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException(id));
+        return mapper.toResponse(customer);
+    }
+
+
+}
